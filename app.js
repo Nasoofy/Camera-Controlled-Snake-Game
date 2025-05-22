3295ec8a23652689ccd7f1103bdce3b5094a4445
Nasif Ahmed <nnasifahmed05@gmail.com>
2025-05-22 15:05:21 -0700

    snake game first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const scoreText = document.getElementById("score");
+const resetbutton = document.getElementById("resetbutton");
+const gameWidth = canvas.width;
+const gameHeight = canvas.height
+const boardBack = "white";
+const snakeColor = "lightgreen";
+const snakeBorder = "black";
+const foodColor = "red";
+const unitSize = 25;
+let running = false;
+let xVelocity = unitSize;
+let yVelocity = 0;
+let foodX;
+let foodY;
+let score = 0;
+let snake = [
+    {x:unitSize*4,y:0},
+    {x:unitSize*3,y:0},
+    {x:unitSize*2,y:0},
+    {x:unitSize,y:0},
+    {x:0, y:0}
+];
+window.addEventListener("keydown",changeDirection);
+resetbutton.addEventListener("click",resetGame);
+function gameStart() {};
+function nextTick(){};
+function clearBoard(){};
+function createFood(){
+    function randomFood(min,max){
+        const randNum = Math.round((Math.random()(max-min)+min))
+        return randNum;
+    }
+};
+function drawFood(){};
+function moveSnake(){};
+function changeDirection(){};
+function checkGameOver(){};
+function displayGameOver(){};
+function resetGame(){};
